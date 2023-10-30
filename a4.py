27078e27b762ca00251bcc02005b9e7735a9376b
Megan Wong <megan.e.wong16@gmail.com>
2023-10-30 11:22:49 -0500

    Write up complete

--- a4.py
+++ a4.py
@@ -10,7 +10,24 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self) -> None:
+        self.board = ['*'] * 9
+       # print(self.board)
+
+    def __str__(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
+
+    def make_move(self, player, pos) -> bool:
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+
+        self.board[pos] = player
+        return True        
+
+    #def has_won()
 
 
 def play_tic_tac_toe() -> None:
@@ -34,7 +51,7 @@ def play_tic_tac_toe() -> None:
     brd = TTTBoard()
     players = ["X", "O"]
     turn = 0
-
+    
     while not brd.game_over():
         print(brd)
         move: str = input(f"Player {players[turn]} what is your move? ")
@@ -61,8 +78,12 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd.board)
+    print(brd)
     brd.make_move("X", 8)
     brd.make_move("O", 7)
+    brd.make_move("O", 8)
+    print(brd)
 
     assert brd.game_over() == False
 
